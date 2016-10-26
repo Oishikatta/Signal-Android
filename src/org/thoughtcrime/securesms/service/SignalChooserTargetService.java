051c5317364ef7f9423fd13d031bbf1d72b49b93
Corey Hunter <Oishikatta+github.com@regex.be>
2016-10-26 15:24:30 -0700

    Refactor SignalChooserTargetService, add copyright/javadoc per code style guidelines
    Refactor SignalChooserTargetService, add copyright/javadoc per code
    style guidelines

--- src/org/thoughtcrime/securesms/service/SignalChooserTargetService.java
+++ src/org/thoughtcrime/securesms/service/SignalChooserTargetService.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2016 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.thoughtcrime.securesms.service;
 
 import android.annotation.TargetApi;
@@ -15,19 +32,21 @@ import android.util.Log;
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.ShareActivity;
 import org.thoughtcrime.securesms.color.MaterialColor;
-import org.thoughtcrime.securesms.contacts.avatars.ContactColors;
 import org.thoughtcrime.securesms.contacts.avatars.ContactPhoto;
-import org.thoughtcrime.securesms.contacts.avatars.ContactPhotoFactory;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.ThreadDatabase;
-import org.thoughtcrime.securesms.recipients.Recipient;
-import org.thoughtcrime.securesms.recipients.RecipientFactory;
+import org.thoughtcrime.securesms.database.model.ThreadRecord;
+import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.util.BitmapUtil;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Class to respond to direct share (chooser target) requests from the system
+ *
+ * @author Corey Hunter
+ */
 @TargetApi(Build.VERSION_CODES.M)
 public class SignalChooserTargetService extends ChooserTargetService {
   private static final String TAG = SignalChooserTargetService.class.getSimpleName();
@@ -43,66 +62,35 @@ public class SignalChooserTargetService extends ChooserTargetService {
     Cursor conversationList = threadDatabase.getConversationList();
 
     if (conversationList.moveToFirst()) {
-      do {
-        Long threadID = conversationList.getLong(
-            conversationList.getColumnIndex(ThreadDatabase.ID));
-        String recipientIds = conversationList.getString(
-            conversationList.getColumnIndex(ThreadDatabase.RECIPIENT_IDS));
-        int distributionType = conversationList.getInt(
-            conversationList.getColumnIndex(ThreadDatabase.TYPE));
-
-        Bundle extras = new Bundle();
-        extras.putBoolean("isDirectShare", true);
-        extras.putLong(ThreadDatabase.ID, threadID);
-        extras.putInt(ThreadDatabase.TYPE, distributionType);
-
-        StringBuilder displayName = new StringBuilder();
-        ContactPhoto contactPhoto = ContactPhotoFactory.getDefaultContactPhoto(null);
-        MaterialColor contactPhotoColor = ContactColors.UNKNOWN_COLOR;
-
-        Iterator<Recipient> recipientIterator = RecipientFactory.getRecipientsForIds(this, recipientIds, false).iterator();
-
-        for (int recipientIdx = 0; recipientIterator.hasNext(); recipientIdx++) {
-          Recipient currentRecipient = recipientIterator.next();
-          String recipientDisplayName = currentRecipient.getName();
-
-          if (recipientDisplayName == null) {
-            recipientDisplayName = currentRecipient.getNumber();
-          }
-
-          displayName.append(recipientDisplayName);
-
-          if (recipientIterator.hasNext()) {
-            displayName.append(", ");
-          }
-
-          if (recipientIdx == 0 && !recipientIterator.hasNext()) {
-            contactPhoto = currentRecipient.getContactPhoto();
-            contactPhotoColor = currentRecipient.getColor();
-          } else if (recipientIdx == 1) {
-            contactPhoto = ContactPhotoFactory.getDefaultGroupPhoto();
-            contactPhotoColor = currentRecipient.getColor();
-          }
-        }
-
-        targets.add(
-            new ChooserTarget(
-                displayName,
-                getIconFromContactPhoto(contactPhoto, contactPhotoColor),
-                1.0f,
-                componentName,
-                extras
-            )
-        );
-
+      ThreadDatabase.Reader threadReader = threadDatabase.readerFor(conversationList, null);
+      for (ThreadRecord thread = threadReader.getCurrent(); thread != null; thread = threadReader.getNext()) {
+        targets.add(getChooserTarget(thread, componentName));
         if (targets.size() >= 8) {
           break;
         }
-      } while (conversationList.moveToNext());
+      }
+    } else {
+      Log.w(TAG, "No open conversations, returning empty direct share target list.");
     }
 
     conversationList.close();
     return targets;
+  }
+
+  private ChooserTarget getChooserTarget(ThreadRecord thread, ComponentName componentName) {
+    Bundle extras = new Bundle();
+    extras.putBoolean("isDirectShare", true);
+    extras.putLong(ThreadDatabase.ID, thread.getThreadId());
+    extras.putInt(ThreadDatabase.TYPE, thread.getDistributionType());
+    Recipients recipients = thread.getRecipients();
+
+    return new ChooserTarget(
+        recipients.toShortString(),
+        getIconFromContactPhoto(recipients.getContactPhoto(), recipients.getColor()),
+        1.0f,
+        componentName,
+        extras
+    );
   }
 
   private Icon getIconFromContactPhoto (ContactPhoto contactPhoto, MaterialColor contactPhotoColor) {
